9d1d6de77d4adbc3c0f6db94c53ac2a1c43377f0
Teegala Saketh <sakethteegala@gmail.com>
2025-07-02 21:50:49 +0530

    updated fuctions on mobile-d

--- public/javascript.js
+++ public/javascript.js
@@ -1,17 +1,21 @@
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
+document.addEventListener("DOMContentLoaded", () => {
+    const bar = document.getElementById('bar');
+    const close = document.getElementById('close');
+    const nav = document.getElementById('navbar');
 
-if (bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
-    });
-}
-if (close) {
-    close.addEventListener('click', () => {
-        nav.classList.remove('active');
-    });
-}
+    if (bar) {
+        bar.addEventListener('click', () => {
+            nav.classList.add('active');
+        });
+    }
+
+    if (close) {
+        close.addEventListener('click', () => {
+            nav.classList.remove('active');
+        });
+    }
+});
+
 
 function viewProduct(id) {
     localStorage.setItem("productId", id);
